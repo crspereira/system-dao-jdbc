bbbb399f7198c186ad3818da5121e16a1478955c
Clayton Pereira <clayton.pereira@outlook.com>
2020-08-18 14:52:51 -0300

    findByDepartment Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -24,6 +27,15 @@ public class Program {
 		System.out.println("==== TEST 1: Seller findById ====");
 		Seller seller2 = sellerDao2.findById(3);
 		System.out.println(seller2);
+		
+		System.out.println("\n==== TEST 2: Seller findByDepartment ====");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao2.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);	
+		}
+		
+		
 		
 	}
 
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,6 +2,7 @@ package model.dao;
 
 import java.util.List;
 
+import model.entities.Department;
 //Classe responsável por definir as operaçãoes de acesso a dados (Acesso ao Banco de Dados) relacinado a Sellers
 //Classe definida como Interface para dar flexibilidade e preservação do Contrato de objetos de acesso a dados
 import model.entities.Seller;
@@ -15,5 +16,6 @@ public interface SellerDao {
 	public void deleteById(Integer id);
 	public Seller findById(Integer id); //retorna o ID ou Null
 	public List<Seller> findAll(); //retorna a lista de Vendedores
-	
+	public List<Seller> findByDepartment(Department department); //Assinatura do metodo que retorna a lista de Departamento
+	 
 }
